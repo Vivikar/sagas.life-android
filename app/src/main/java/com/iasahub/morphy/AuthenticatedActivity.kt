8e5f88ddae89360a85288e0f3e80fe8928289b3e
Vladyslav Zalevskyi <vzalevskyi24@gmail.com>
2019-10-20 20:30:59 +0300

    Add facebook pic

--- app/src/main/java/com/iasahub/morphy/AuthenticatedActivity.kt
+++ app/src/main/java/com/iasahub/morphy/AuthenticatedActivity.kt
@@ -5,14 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import com.facebook.AccessToken
-import com.facebook.GraphRequest
-import com.facebook.HttpMethod
 import com.facebook.login.LoginManager
 import com.facebook.share.widget.ShareDialog
 
+import android.widget.TextView
+import com.facebook.*
 
 
+import org.json.JSONException
+
+import com.facebook.GraphRequest
+import com.facebook.AccessToken
+import kotlinx.android.synthetic.main.activity_authenticated.*
+
+import android.widget.ImageView
+
+import com.bumptech.glide.Glide
 
 
 class AuthenticatedActivity : AppCompatActivity() {
@@ -23,6 +31,7 @@ class AuthenticatedActivity : AppCompatActivity() {
     private var imageUrl: String? = null
     //private val TAG = "AuthenticatedActivity"
 
+    private var callbackManager: CallbackManager? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,10 +40,26 @@ class AuthenticatedActivity : AppCompatActivity() {
 
         var btnLogout = findViewById<Button>(R.id.btnLogout)
 
+
+        val accessToken = AccessToken.getCurrentAccessToken()
+        var lg_toekn = findViewById<TextView>(R.id.lg_toekn)
+        var lg_email = findViewById<TextView>(R.id.lg_email)
+        var lg_image = findViewById<ImageView>(R.id.fb_image)
+        if (!accessToken.isExpired){
+            useLoginInformation(accessToken)
+
+        }
+
+
+
+
         btnLogout.setOnClickListener(View.OnClickListener {
             // Logout
             if (AccessToken.getCurrentAccessToken() != null) {
                 GraphRequest(AccessToken.getCurrentAccessToken(), "/me/permissions/", null, HttpMethod.DELETE, GraphRequest.Callback {
+
+
+
                     AccessToken.setCurrentAccessToken(null)
                     LoginManager.getInstance().logOut()
 
@@ -44,5 +69,43 @@ class AuthenticatedActivity : AppCompatActivity() {
                 }).executeAsync()
             }
         })
+    }
+
+    private fun useLoginInformation(accessToken:AccessToken) {
+        /**
+         * Creating the GraphRequest to fetch user details
+         * 1st Param - AccessToken
+         * 2nd Param - Callback (which will be invoked once the request is successful)
+         */
+        val request = GraphRequest.newMeRequest(accessToken
+        ) { `object`, response ->
+            //OnCompleted is invoked once the GraphRequest is successful
+            try {
+                val name = `object`.getString("name")
+                val email = `object`.getString("email")
+                val image_url = `object`.getJSONObject("picture").getJSONObject("data").getString("url")
+                //displayName.setText(name)
+
+
+                var userId = AccessToken.getCurrentAccessToken().userId
+                Glide.with(this)
+                    .load("https://graph.facebook.com/" + userId +  "/picture?type=large")
+                    .into(fb_image);
+
+
+                lg_email.setText(email)
+                lg_toekn.setText(accessToken.token)
+                //fb_image.setImageURI()
+
+            } catch (e:JSONException) {
+                e.printStackTrace()
+            }
+        }
+        // We set parameters to the GraphRequest using a Bundle.
+        val parameters = Bundle()
+        parameters.putString("fields", "id,name,email,picture.width(200)")
+        request.parameters = parameters
+        // Initiate the GraphRequest
+        request.executeAsync()
     }
 }
--- app/src/main/java/com/iasahub/morphy/MainActivity.kt
+++ app/src/main/java/com/iasahub/morphy/MainActivity.kt
@@ -6,13 +6,13 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import com.facebook.CallbackManager
+import com.facebook.*
 
 
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
+import kotlinx.android.synthetic.main.activity_authenticated.*
+import org.json.JSONException
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -25,6 +25,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var loginButton = findViewById<Button>(R.id.login_button)
+
+
+
 
         loginButton.setOnClickListener(View.OnClickListener {
             // Login
@@ -49,6 +52,42 @@ class MainActivity : AppCompatActivity() {
                 })
         })
     }
+
+
+    private fun useLoginInformation(accessToken: AccessToken) {
+        /**
+         * Creating the GraphRequest to fetch user details
+         * 1st Param - AccessToken
+         * 2nd Param - Callback (which will be invoked once the request is successful)
+         */
+        val request = GraphRequest.newMeRequest(accessToken
+        ) { `object`, response ->
+            //OnCompleted is invoked once the GraphRequest is successful
+            try {
+                val name = `object`.getString("name")
+                val email = `object`.getString("email")
+                val image = `object`.getJSONObject("picture").getJSONObject("data").getString("url")
+
+
+                //displayName.setText(name)
+
+
+                lg_email.setText(email)
+                lg_toekn.setText(accessToken.token)
+
+
+            } catch (e: JSONException) {
+                e.printStackTrace()
+            }
+        }
+        // We set parameters to the GraphRequest using a Bundle.
+        val parameters = Bundle()
+        parameters.putString("fields", "id,name,email,picture.width(200)")
+        request.parameters = parameters
+        // Initiate the GraphRequest
+        request.executeAsync()
+    }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
