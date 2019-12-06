ba32ed0fa532e7fc863639a351e86eb85e019518
Vladyslav Zalevskyi <vzalevskyi24@gmail.com>
2019-12-06 16:58:32 +0200

    Add SplashScreen

---
+++ app/src/main/java/com/iasahub/sagas_life/SplashActivity.kt
@@ -0,0 +1,38 @@
+package com.iasahub.sagas_life
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // убедитесь, что вызываете до super.onCreate()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        scheduleSplashScreen()
+
+        /*
+        val user = UserDb.getCurrentUser()
+        routeToAppropriatePage(user)
+        finish()
+         */
+    }
+
+
+    private fun scheduleSplashScreen() {
+        val splashScreenDuration = getSplashScreenDuration()
+        Handler().postDelayed(
+            {
+                // После Splash Screen перенаправляем на нужную Activity
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+                finish()
+            },
+            splashScreenDuration
+        )
+    }
+
+    private fun getSplashScreenDuration() = 5000L
+}
